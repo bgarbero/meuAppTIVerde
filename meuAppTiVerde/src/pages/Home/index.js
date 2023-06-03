d5e6ed61b6af2277d16155a95e21e101e08c47b4
bgarbero <bruno_garbero@hotmail.com>
2023-06-03 17:46:43 -0300

    iniciando a criação das outras pages

--- meuAppTiVerde/src/pages/Home/index.js
+++ meuAppTiVerde/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, StyleSheet, View} from "react-native";
+import {Text, StyleSheet, View,Image} from "react-native";
 import SuperButton from "../../components/superButton";
 
 function Home (){
@@ -30,7 +30,7 @@ const style = StyleSheet.create({
         //marginBottom: 100,
         opacity: 70,
         top: 120
-    }
+    },
 });
 
 export default Home;
